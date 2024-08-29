d5d4606586e650031d8f23562e6a23d30cad049b
Luke <gregorl@ethz.ch>
2024-08-29 19:08:59 +0200

    yaml file changes with job launches

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ls -laF /myhome/cryogrid/
-echo ===========
-ls -laF /home/mambauser/
 /opt/conda/bin/python \
     /home/mambauser/cryogrid-era5-downloader/cli.py \
     /home/mambauser/cryogrid-era5-downloader/requests/cryogrid_pamir_region.yml
--- submit_pamir_jobs.sh
+++ submit_pamir_jobs.sh
@@ -1,12 +1,14 @@
 
-for YEAR_START in $(seq 1961 8 2024); do
+for YEAR_START in $(seq 1960 8 2024); do
 
     YEAR_END=$(($YEAR_START+7))
     export YEAR_START
     export YEAR_END
     export DOCKER_TAG=$YEAR_START-$YEAR_END
+    
+    sed -i.bak -e "s/\(start_year: \)[0-9]\{4\}/\1$YEAR_START/" -e "s/\(end_year: \)[0-9]\{4\}/\1$YEAR_END/" ./requests/cryogrid_pamir_region.yml
+    
     make build DOCKER_BUILD_FLAGS='--no-cache'
     make push
     make runai-submit
-    break
 done
